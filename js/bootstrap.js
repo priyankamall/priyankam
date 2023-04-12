09ac7c2a101696f0c17adac44b2272fdb8596a00
Priyanka Mall <87891538+priyankamall@users.noreply.github.com>
2023-04-12 10:14:11 +0530

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -20,8 +20,6 @@ if (typeof jQuery === 'undefined') {
  * Copyright 2011-2015 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
-
-
 +function ($) {
   'use strict';
 
