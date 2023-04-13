3768dd099900438899d5ad85612daeb22cdaf5d9
Priyanka Mall <87891538+priyankamall@users.noreply.github.com>
2023-04-13 08:25:45 +0530

    Update google-maps.js

--- js/google-maps.js
+++ js/google-maps.js
@@ -31,7 +31,6 @@ function init() {
     // Get the HTML DOM element that will contain your map 
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
-
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
